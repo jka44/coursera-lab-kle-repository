d8fcf845bf9f54dc67af61184045337f9aa07a7d
Kevin Lemoine <kevin.lemoine@iadvize.com>
2025-08-20 10:00:11 -0400

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -11,7 +11,7 @@ if (userRole === "admin") {
 
 console.log ("Access Level =",accessLevel);
 
-let isLoggedIn = false;
+let isLoggedIn = true;
 let userMessage;
 
 if (isLoggedIn){
@@ -24,7 +24,29 @@ if (isLoggedIn){
     userMessage = "Please log in to the system";
 }
 
-console.log ("User message ",userMessage);
+console.log ("User message =",userMessage);
 
 let userType = "subscriber";
 let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log ("user category =", userCategory);
+
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log ("Authentication Status =", authenticationStatus);
